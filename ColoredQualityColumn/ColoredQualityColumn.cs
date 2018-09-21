de42e746877f8bfd2d2915bed4f76405eac50dec
CutthroatBaron <43160823+CutthroatBaron@users.noreply.github.com>
2018-09-21 00:22:52 -0400

    Initial commit

---
+++ ColoredQualityColumn/ColoredQualityColumn.cs
@@ -0,0 +1,223 @@
+﻿/*
+  KeePass ColoredQualityColumn Plugin
+*/
+
+using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Windows.Forms;
+using System.Diagnostics;
+using System.Drawing;
+
+using KeePass.Forms;
+using KeePass.Plugins;
+using KeePass.UI;
+using KeePass.Util.Spr;
+
+using KeePassLib;
+using KeePassLib.Cryptography;
+using KeePassLib.Utility;
+using KeePassLib.Security;
+
+namespace ColoredQualityColumn
+{
+    public sealed class ColoredQualityColumnExt : Plugin
+    {
+        private static IPluginHost m_host = null;
+        private ColoredQualityColumnProvider m_prov = null;
+        //Quality classification cutoffs, populated per KeePass website.
+        //In the future, might make these configurable.
+        private SortedList<uint, Color> QualityDelimiter = new SortedList<uint, Color> {
+            {             0, Color.FromArgb(unchecked((int)0xFFFFFFFF)) },
+            {            64, Color.FromArgb(unchecked((int)0xFFFF6961)) },
+            {            80, Color.FromArgb(unchecked((int)0xFFFFB347)) },
+            {           112, Color.FromArgb(unchecked((int)0xFFFDFD96)) },
+            {           128, Color.FromArgb(unchecked((int)0xFFCCFF99)) },
+            { uint.MaxValue, Color.FromArgb(unchecked((int)0xFF77DD77)) },
+        };
+        private const string QcpName = "Quality";
+
+        internal static IPluginHost Host
+        {
+            get { return m_host; }
+        }
+
+        public override bool Initialize(IPluginHost host)
+        {
+            Terminate();
+
+            m_host = host;
+            if (m_host == null) { Debug.Assert(false); return false; }
+
+            m_prov = new ColoredQualityColumnProvider();
+            m_host.ColumnProviderPool.Add(m_prov);
+
+            ListView lv = (m_host.MainWindow.Controls.Find(
+                "m_lvEntries", true)[0] as ListView);
+            if (lv == null) { Debug.Assert(false); return false; }
+
+            //Custom draw the entry list so we can set background color.
+            lv.OwnerDraw = true;
+            lv.DrawColumnHeader += Lv_DrawColumnHeader;
+            lv.DrawItem += Lv_DrawItem;
+            lv.DrawSubItem += Lv_DrawSubItem;
+
+            m_host.MainWindow.FileClosed += this.OnFileClosed;
+
+            return true;
+        }
+
+        public override void Terminate()
+        {
+            if (m_host == null) return;
+
+            m_host.MainWindow.FileClosed -= this.OnFileClosed;
+
+            ListView lv = (m_host.MainWindow.Controls.Find(
+                "m_lvEntries", true)[0] as ListView);
+            if (lv == null) { Debug.Assert(false); return; }
+
+            lv.DrawSubItem -= Lv_DrawSubItem;
+            lv.DrawItem -= Lv_DrawItem;
+            lv.DrawColumnHeader -= Lv_DrawColumnHeader;
+            lv.OwnerDraw = false;
+
+            m_host.ColumnProviderPool.Remove(m_prov);
+            m_prov = null;
+
+            m_host = null;
+        }
+
+        private void Lv_DrawItem(object sender, DrawListViewItemEventArgs e)
+        {
+            e.Item.UseItemStyleForSubItems = false;
+            e.DrawDefault = true;
+        }
+
+        private void Lv_DrawColumnHeader(object sender, DrawListViewColumnHeaderEventArgs e)
+        {
+            e.DrawDefault = true;
+        }
+
+        private void Lv_DrawSubItem(object sender, DrawListViewSubItemEventArgs e)
+        {
+            ListViewItem lvi = e.Item;
+            if (e.Header.Text == QcpName)
+            {
+                PwListItem li = (lvi.Tag as PwListItem);
+                if (li == null) { Debug.Assert(false); return; }
+
+                PwEntry pe = li.Entry;
+                if (pe == null) { Debug.Assert(false); return; }
+
+                ProtectedString pStr = pe.Strings.Get(PwDefs.PasswordField);
+                if (pStr == null) { Debug.Assert(false); return; }
+
+                string strPw = pStr.ReadString();
+
+                uint uCacheEst = ColoredQualityColumnProvider.loadQualityFromCache(strPw);
+                foreach (KeyValuePair<uint, Color> kvp in QualityDelimiter)
+                {
+                    if (uCacheEst <= kvp.Key)
+                    {
+                        e.SubItem.BackColor = kvp.Value;
+                        break;
+                    }
+                }
+            }
+            else
+            {
+                e.SubItem.BackColor = lvi.BackColor;
+            }
+            e.DrawDefault = true;
+        }
+
+        private void OnFileClosed(object sender, FileClosedEventArgs e)
+        {
+            ColoredQualityColumnProvider.ClearCache();
+        }
+    }
+
+    public sealed class ColoredQualityColumnProvider : ColumnProvider
+    {
+        private const string QcpName = "Quality";
+        private const string QcpBitsSuffix = " bits";
+
+        private static object m_oCacheSync = new object();
+        private static Dictionary<string, uint> m_dCache =
+            new Dictionary<string, uint>();
+
+        private string[] m_vColNames = new string[] { QcpName };
+        public override string[] ColumnNames
+        {
+            get { return m_vColNames; }
+        }
+
+        public override HorizontalAlignment TextAlign
+        {
+            get { return HorizontalAlignment.Right; }
+        }
+
+        internal static void ClearCache()
+        {
+            lock (m_oCacheSync)
+            {
+                m_dCache.Clear();
+            }
+        }
+
+        internal static uint loadQualityFromCache(String pw)
+        {
+            uint pwStrength = 0;
+            if (m_dCache.ContainsKey(pw))
+            {
+                pwStrength = m_dCache[pw];
+            }
+            return pwStrength;
+        }
+
+        public override string GetCellData(string strColumnName, PwEntry pe)
+        {
+            if (strColumnName == null) { Debug.Assert(false); return string.Empty; }
+            if (strColumnName != QcpName) return string.Empty;
+            if (pe == null) { Debug.Assert(false); return string.Empty; }
+
+            string strPw = pe.Strings.ReadSafe(PwDefs.PasswordField);
+
+            if (strPw.IndexOf('{') >= 0)
+            {
+                IPluginHost host = ColoredQualityColumnExt.Host;
+                if (host == null) { Debug.Assert(false); return string.Empty; }
+
+                PwDatabase pd = null;
+                try
+                {
+                    pd = host.MainWindow.DocumentManager.SafeFindContainerOf(pe);
+                }
+                catch (Exception) { Debug.Assert(false); }
+
+                SprContext ctx = new SprContext(pe, pd, (SprCompileFlags.Deref |
+                    SprCompileFlags.TextTransforms), false, false);
+                strPw = SprEngine.Compile(strPw, ctx);
+            }
+
+            uint uEst;
+            lock (m_oCacheSync)
+            {
+                if (!m_dCache.TryGetValue(strPw, out uEst)) uEst = uint.MaxValue;
+            }
+
+            if (uEst == uint.MaxValue)
+            {
+                uEst = QualityEstimation.EstimatePasswordBits(strPw.ToCharArray());
+
+                lock (m_oCacheSync)
+                {
+                    m_dCache[strPw] = uEst;
+                }
+            }
+
+            return (uEst.ToString() + QcpBitsSuffix);
+        }
+    }
+}
---
+++ ColoredQualityColumn/Properties/AssemblyInfo.cs
@@ -0,0 +1,36 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+// General Information about an assembly is controlled through the following 
+// set of attributes. Change these attribute values to modify the information
+// associated with an assembly.
+[assembly: AssemblyTitle("ColoredQualityColumn")]
+[assembly: AssemblyDescription("Provides a color coded 'Password Quality' column. Uses 'QualityColumn' plugin as codebase.")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("Garrett Denham")]
+[assembly: AssemblyProduct("KeePass Plugin")]
+[assembly: AssemblyCopyright("Copyright © 2017")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+// Setting ComVisible to false makes the types in this assembly not visible 
+// to COM components.  If you need to access a type in this assembly from 
+// COM, set the ComVisible attribute to true on that type.
+[assembly: ComVisible(false)]
+
+// The following GUID is for the ID of the typelib if this project is exposed to COM
+[assembly: Guid("701ceb82-aba9-4495-8edc-413e5f2ef84a")]
+
+// Version information for an assembly consists of the following four values:
+//
+//      Major Version
+//      Minor Version 
+//      Build Number
+//      Revision
+//
+// You can specify all the values or you can default the Build and Revision Numbers 
+// by using the '*' as shown below:
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
